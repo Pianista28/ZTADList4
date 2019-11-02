4d0395bb897df6ea0e96eaa1aa8e1e5148e561d7
Pianista28 <jan.borowiak.jb8@gmail.com>
2019-11-02 13:32:26 +0100

    Add Excercise2 solution

---
+++ Excercise2.R
@@ -0,0 +1,17 @@
+nerwowi <- c(3, 3, 4, 5, 5)
+spokojni <- c(4, 6, 7, 9, 9)
+
+swNerwowi <- shapiro.test(nerwowi)
+swSpokojni <- shapiro.test(spokojni)
+#both are normal distribution
+
+varEqual <- var.test(nerwowi, spokojni)
+var.test(nerwowi, spokojni)
+#variance equal
+
+#We can perform ttest
+
+ttestResult1 <- t.test(nerwowi, spokojni, var.equal = TRUE, conf.level = 0.95, alternative = c("greater"))
+ttestResult1
+
+#We don't have grounds to reject the hypothesis that means are equal
